125034bf344295de746546a59eb36565b77dc5ea
Moelaal <medo.elaal@gmail.com>
2022-06-25 05:26:25 +0200

    adding stars coponent

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,7 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 const Stars = ({ stars, reviews }) => {
-  return <Wrapper></Wrapper>;
+  const revStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+  return (
+    <Wrapper>
+      <div className="stars">{revStars}</div>
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
